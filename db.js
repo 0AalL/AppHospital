441408053df558e2e774d9a985336b8f078c0fe5
0AalL <alejosanchez456@gmail.com>
2025-04-18 18:54:00 -0500

    creacion

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'safebryan',
+  password: '081012',
+  database: 'consultas_medicas',
+  port: 3308
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = db;
