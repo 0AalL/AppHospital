6d7793ed6cb14a6e44431ef2eb0f71e5e6fc62a3
0AalL <alejosanchez456@gmail.com>
2025-04-18 19:49:50 -0500

    creacion

--- migrations/20250419004315-create-usuario.js
+++ migrations/20250419004315-create-usuario.js
@@ -1,6 +1,12 @@
 // migrations/xxxxxx-create-usuario.js
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // Hashear la contraseña "1234"
+    const hashedPassword = await bcrypt.hash('1234', 10);
+
+    // Crear la tabla de usuarios
     await queryInterface.createTable('usuarios', {
       id: {
         type: Sequelize.BIGINT,
@@ -32,6 +38,17 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
     });
+
+    // Insertar un usuario con la contraseña hasheada "1234"
+    await queryInterface.bulkInsert('usuarios', [
+      {
+        nombre_usuario: 'admin',
+        contrasena: hashedPassword,
+        tipo_usuario: 'administrador',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
